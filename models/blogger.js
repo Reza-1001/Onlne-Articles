b04963b91c7f3df393285d586b73fc92d569eb0c
Reza Ahmad Nezhad <resistance1271@gmail.com>
2021-03-30 16:18:14 +0430

    password encryption added

--- models/blogger.js
+++ models/blogger.js
@@ -50,6 +50,20 @@ const essentialSchema = {
           trim:true
       }
   })
+  bloggerShcema.pre('save',function(next){
+    const user=this;
+    if (user.isNew || user.isModified('password')){
+      bcrypt.genSalt(10,function(err,salt){
+        if (err) return next(err);
+        bcrypt.hash(user.password,salt,function(err,hash){
+          if (err) return next(err);
+          user.password=hash;
+          return next();
+        })
+      })
+    }
+    else return next();
+  })
   const Blogger = mongoose.model("Blogger", bloggerShcema);
 
   module.exports = Blogger;
