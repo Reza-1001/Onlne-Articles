7300429396148af9d86d6ba2b94573bc0bfce2e6
Reza Ahmad Nezhad <resistance1271@gmail.com>
2021-06-03 02:28:33 +0430

    login require added

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const register=require('../controller/register');
+const register = require('../controller/register');
+const login = require('../controller/login');
 
 router.use('/register', register);
 
 router.use('/login', login);
 
 
-module.exports = router;
+module.exports = router; 
