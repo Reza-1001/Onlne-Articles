e73e7a76886dfebbfc650f87b9394b4e20df5ed2
Reza Ahmad Nezhad <resistance1271@gmail.com>
2021-06-03 02:48:13 +0430

    login service created

---
+++ services/login.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const Blogger=require('../models/blogger');
+
+const loginPage = (req, res, next) => {
+    res.render('./pages/login', {
+        error: null
+    })
+}
+
+
+const bloggerLogin = (req, res, next) => {
+    console.log(req.body)
+    if (!req.body.userName || !req.body.password) {
+        return res.render('pages/login', {
+            error: "Empty Fields"
+        })
+    }
+    Blogger.findOne({
+        "userName": req.body.userName
+    }, (err, user) => {
+        console.log(user)
+        if (err) return res.render('pages/login', {
+            error: "Server Error"
+        })
+        if (!user) {
+            return res.render('pages/login', {
+                error: "User Not Found"
+            })
+        }
+        bcrypt.compare(req.body.password, user.password, (err, isMatch) => {
+            if (err) return res.render('pages/login', {
+                error: "Server Error"
+            })
+            if (!isMatch) return res.render('pages/login', {
+                error: "User Not Found"
+            })
+            req.session.user = user
+            // res.redirect('/api/user/dashboard')
+            res.send('Enter')
+        })
+
+    })
+}
+
+module.exports={loginPage,bloggerLogin};
