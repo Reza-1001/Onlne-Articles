52152f712e6278ed2a5bf2e5818dbf1b743427c4
Reza Ahmad Nezhad <resistance1271@gmail.com>
2021-06-04 11:51:12 +0430

    update user info

--- controller/users.js
+++ controller/users.js
@@ -2,15 +2,19 @@ const express = require('express');
 const router = express.Router();
 const generalTools=require('../tools/general-tools');
 const {IsAdmin}=require('../tools/general-tools');
+
 const {
     getAllUsers,
-    updateBlogger
+    updateUserPassword,
+    updateUserInfo
 } = require('../services/users');
-
 
 
 router.get('/', IsAdmin, getAllUsers);
 
-router.patch('/', generalTools.PasswordCheck,updateBlogger)
+router.patch('/', generalTools.PasswordCheck,updateUserPassword)
+
+router.put('/',updateUserInfo)
+
 
 module.exports = router;
--- services/users.js
+++ services/users.js
@@ -5,28 +5,43 @@ const Blogger = require('../models/users');
 const Article = require('./../models/article');
 const generalTools = require('./../tools/general-tools.js');
 
-const getAllUsers=(req,res,next)=>{
-    Blogger.find({role:'blogger'},(err,bloggers)=>{
+const getAllUsers = (req, res, next) => {
+    Blogger.find({
+        role: 'blogger'
+    }, (err, bloggers) => {
         if (err) return res.send("Server Error")
         if (!bloggers) return res.send("No Blogger Found")
-         
+
         // res.render('pages/dashboard',{users:bloggers})
         return res.send(bloggers);
     })
 }
 
-const updateBlogger=(req,res,next) => {
+const updateUserPassword = (req, res, next) => {
     Blogger.updateOne({
         _id: req.session.user._id
-      }, {
+    }, {
         $set: req.body
-      }, {
+    }, {
         new: true
-      }, (err, user) => {
+    }, (err, user) => {
         if (err) return res.status(500).send("Somthing went wrong in update user! \n" + err);
         return res.json(true)
-      })
+    })
+}
+
+const updateUserInfo = (req, res, next) => {
+    Blogger.findByIdAndUpdate(req.session.user._id, req.body, {
+        new: true
+    }, (err, user) => {
+        if (err) return res.status(500).json(false);
+        res.json(true);
+    })
 }
 
 
-module.exports={getAllUsers,updateBlogger};
+module.exports = {
+    getAllUsers,
+    updateUserPassword,
+    updateUserInfo
+};
