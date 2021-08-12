ad906b9dea6b7bb6e8a2cbaf247bf4565bea3c94
Reza Ahmad Nezhad <resistance1271@gmail.com>
2021-08-12 16:13:39 +0430

    check for article folder exist in public and create

--- tools/initial.js
+++ tools/initial.js
@@ -5,5 +5,5 @@ const path=require('path');
 module.exports=(function(){
  (fs.existSync(path.join(__dirname, '../public/images')) || fs.mkdirSync(path.join(__dirname, '../public/images')))
  (fs.existSync(path.join(__dirname, '../public/images/avatar')) || fs.mkdirSync(path.join(__dirname, '../public/images/avatar')))
-
+ (fs.existSync(path.join(__dirname, '../public/articles')) || fs.mkdirSync(path.join(__dirname, '../public/articles')))
 })    
